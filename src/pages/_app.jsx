6e46b19c08d16bb324f880fd2323fc137647bbe3
DiegoCapella <diegocapelladev@gmail.com>
2022-05-04 14:13:50 -0300

    Add Fonts & Condig GlobalStyles

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from '../styles/global'
+import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Linux Command</title>
+        <meta name="theme-color" content="#323741" />
+        <meta
+          name="description"
+          content="Setup de Instalação de Programas Linux"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
